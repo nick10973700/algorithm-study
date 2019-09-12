30b14e0066ac977761c9828ec395bcb5a018aa09
Takagi <209098532@qq.com>
2019-09-12 14:56:33 +0800

    20181201 小明上学

---
+++ src/t2018_12_1/Main.java
@@ -0,0 +1,35 @@
+package t2018_12_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int r = input.nextInt();
+        int y = input.nextInt();
+        int g = input.nextInt();
+
+        //总时间
+        int time = 0;
+
+        int instance = input.nextInt();
+        int k,t;
+        for (int i = 0; i < instance; i++) {
+            k = input.nextInt();
+            t = input.nextInt();
+            switch (k) {
+                case 0:
+                    time += t;
+                    break;
+                case 1:
+                    time += t;
+                    break;
+                case 2:
+                    time += t + r;
+                case 3:
+                    break;
+            }
+        }
+        System.out.println(time);
+    }
+}
